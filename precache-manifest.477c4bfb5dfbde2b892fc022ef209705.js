1d267fa5a636b2e1108f123e708b8b01126ca5f4
王森 <279872412@qq.com>
2020-07-28 17:29:39 +0800

    Add files via upload

---
+++ precache-manifest.477c4bfb5dfbde2b892fc022ef209705.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5325873ac89f876fff66",
+    "url": "/css/app.9591bef0.css"
+  },
+  {
+    "revision": "71b753568625d7cc18c8",
+    "url": "/css/chunk-vendors.2ac5db4b.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "9a71b5572a81140ff636fe76670499c2",
+    "url": "/img/login_bg_01.9a71b557.png"
+  },
+  {
+    "revision": "6376266b90d2a0facf45cc3b81dcb162",
+    "url": "/img/n1.6376266b.jpg"
+  },
+  {
+    "revision": "3125cad5f935822e1285425e659c604b",
+    "url": "/img/n2.3125cad5.jpg"
+  },
+  {
+    "revision": "76618c09d8819cfb1e8397d6918453e7",
+    "url": "/img/n3.76618c09.jpg"
+  },
+  {
+    "revision": "dc2d0d51198227661235c4a5c3b1c525",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5325873ac89f876fff66",
+    "url": "/js/app.a88d0d7b.js"
+  },
+  {
+    "revision": "71b753568625d7cc18c8",
+    "url": "/js/chunk-vendors.4397c053.js"
+  },
+  {
+    "revision": "7c51609a42f2f1a0466233be60abab68",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
